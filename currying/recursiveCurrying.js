eeeb946e0b28f058fa00ca5a1f209faf31b8395a
StivenBermeo <hsbermeo@misena.edu.co>
2021-05-16 22:22:03 -0500

    smoother non OOP ugly recursive currying (doesn't require func.length)

--- currying/recursiveCurrying.js
+++ currying/recursiveCurrying.js
@@ -1,19 +1,27 @@
 //? use lodash, this already exists try to mutate only if necessary(couldn't bother to look for the function in it's github page)
 
+
 const spacing = function(){
-  console.log(Object.values(arguments).join(' '));
+  const args  = [...Object.values(arguments)];
+  const limit = args.splice(-1);
+
+  if(args.length >= limit){
+    console.log(args.join(' '));
+  }
+  
 }
 
-const recCurrying = function(fn, params = []){
-  return (newArg)=>{ 
-    params = [...params,newArg]   
-    fn.apply(null,params);
-    return recCurrying(fn,params);
+const recCurrying = function(fn, _params = [], num){
+  return (newArg, _alterNumAtAnyPoint = false)=>{ 
+    let params = [..._params,newArg]   
+    fn.apply(null, [...params, _alterNumAtAnyPoint? _alterNumAtAnyPoint :  num]);
+    return recCurrying(fn,params,num);
   }
 }
 
-const spacingCurrying = recCurrying(spacing);
-const potentialCurryingMeme =  spacingCurrying('this')('is') ;
-spacingCurrying('some');
-spacingCurrying('lame')('text');
-potentialCurryingMeme('Sparta !');
+let spacingCurryingVI = recCurrying(spacing,[],6);
+const potentialCurryingMeme = spacingCurryingVI = spacingCurryingVI('this')('is') ;
+spacingCurryingVI = spacingCurryingVI('some');
+spacingCurryingVI = spacingCurryingVI('lame')('text');
+spacingCurryingVI = spacingCurryingVI('/dot for testing purposes/ .');
+potentialCurryingMeme('Sparta !',3);
