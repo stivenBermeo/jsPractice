9631b1e43e49a26c68e2b3a8d63a14cc8546215f
StivenBermeo <hsbermeo@misena.edu.co>
2021-05-16 21:23:57 -0500

    Smoother incertain # currying

--- currying/recursiveCurrying.js
+++ currying/recursiveCurrying.js
@@ -1,38 +1,19 @@
 //? use lodash, this already exists try to mutate only if necessary(couldn't bother to look for the function in it's github page)
 
-
 const spacing = function(){
   console.log(Object.values(arguments).join(' '));
 }
 
-class Currying {
-
-  params = [];
-  fn = null;
-  constructor(fn){
-    this.fn = fn;
+const recCurrying = function(fn, params = []){
+  return (newArg)=>{ 
+    params = [...params,newArg]   
+    fn.apply(null,params);
+    return recCurrying(fn,params);
   }
-
-  curry(val){
-    this.params.push(val);
-    return (newArg)=>{
-      return this.curry(newArg)
-    };
-  }
-
-  exec(){
-    this.fn.apply(null, this.params);
-  }
-
 }
 
-const spaceCurrying = new Currying(spacing);
-
-const x = spaceCurrying.curry('this')('is');
-spaceCurrying.curry('text');
-spaceCurrying.exec();
-
-spaceCurrying.curry('it also is')('-kinda-')('-fun tho\'-' );
-spaceCurrying.curry('crappy code for practice');
-spaceCurrying.exec();
-
+const spacingCurrying = recCurrying(spacing);
+const potentialCurryingMeme =  spacingCurrying('this')('is') ;
+spacingCurrying('some');
+spacingCurrying('lame')('text');
+potentialCurryingMeme('Sparta !');
